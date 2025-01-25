84471cf8a023e7d52c553d1add3c11f95e5588fc
Solemarc <larco9mason@gmail.com>
2025-01-25 19:22:21 +1000

    simplify bash code

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # create the build folder
-if [ ! -d build ]; then
-  mkdir build
+if [ ! -d build ]
+  then mkdir build
 fi
 
 # build the server
 dart compile exe main.dart
 # move the executable into the build folder
-if [ -f main.exe ]; then
-  mv ./main.exe build/server
+if [ -f main.exe ]
+  then mv ./main.exe build/server
 fi
 
 # clone static files into the same directory as executable
